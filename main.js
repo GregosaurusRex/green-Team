1a861eeee466f9157b4e8e43840cd5d142cfd98d
GregosaurusRex <cat_dude99@hotmail.com>
2016-05-30 20:34:15 +0930

    Added drawmap - Does not work yet

--- main.js
+++ main.js
@@ -2,6 +2,40 @@
 var player = new Player();
 var keyboard = new Keyboard();
 
+var LAYER_COUNT = 2;
+var MAP = { tw: 60, th: 15};
+var TILE = 35;
+var TILESET_TILE = TILE * 2;
+var TILESET_PADDING = 2;
+var TILESET_SPACING = 2;
+var TILESET_COUNT_X = 14;
+var TILESET_COUNT_Y = 14;
+
+var tileset = document.createElement("img");
+tileset.src = "tileset.png";
+
+function drawMap()
+{
+    for(var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++)
+    {
+        var idx = 0;
+        for(var y = 0; y < level1.layers[layerIdx].height; y++)
+        {
+            for(var x = 0; x < level1.layers[layerIdx].width; x++)
+            {
+                if(level1.layers[layerIdx].data[idx] != 0)
+                {
+                    var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+                    var sx = TILESET_PADDING + (tileInded % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
+                    var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
+                    context.drawImage(tileset,sx,sy,TILESET_TILE,TILESET_TILE,x*TILE,(y-1)*TILE,TILESET_TILE,TILESET_TILE);
+                }
+                idx++;
+            }
+        }
+    }
+}
+
 function run()
 {
     context.fillStyle = "#ccc";
