bb3e5a72475244977c8a4a5de735f37804bbfb3c
ninjaboy1237 <BRA0006@TC.vic.edu.au>
2016-06-08 20:05:16 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,214 @@
+var startFrameMillis = Date.now();
+var endFrameMillis = Date.now();
+
+function getDeltaTime()
+{
+	endFrameMillis = startFrameMillis;
+	startFrameMillis = Date.now();
+
+	var deltaTime = (startFrameMillis - endFrameMillis) * 0.001;
+
+	if(deltaTime > 1)
+		deltaTime = 1;
+
+	return deltaTime;
+}
+
+var LAYER_COUNT = 2;
+var MAP = { tw: 50, th: 20 };
+var TILE = 35;
+var TILESET_TILE = TILE * 2;
+var TILESET_PADDING = 2;
+var TILESET_SPACING = 2;
+var TILESET_COUNT_X = 14;
+var TILESET_COUNT_Y = 14;
+
+var LAYER_BACKGROUND = 1;
+var LAYER_PLATFORMS = 0;
+//var LAYER_LADDERS = 2;
+
+var METER = TILE;
+var GRAVITY = METER * 9.8 * 6;
+var MAXDX = METER * 10;
+var MAXDY = METER * 15;
+var ACCEL = MAXDX * 2;
+var FRICTION = MAXDX * 6;
+var JUMP = METER * 1500;
+
+var player = new Player();
+var keyboard = new Keyboard();
+
+
+var tileset = document.createElement("img");
+tileset.src = "tileset.png";
+
+var worldOffsetX =0;
+function drawMap()
+{
+    var startX = -1;
+    var maxTiles = Math.floor(SCREEN_WIDTH/ TILE) + 2;
+    var tileX = pixelToTile(player.position.x);
+    var offsetX = TILE + Math.floor(player.position.x%TILE);
+
+    startX = tileX - Math.floor(maxTiles / 2);
+    
+    if(startX <- 1)
+    {
+        startX = 0;
+        offsetX = 0;
+    }
+    if(startX > MAP.tw - maxTiles)
+    {
+        startX = MAP.tw - maxTiles + 1;
+        offsetX = TILE;
+    }
+    
+    worldOffsetX = startX * TILE + offsetX;
+    
+    for(var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++)
+    {
+        //var idx = 0;
+        for(var y = 0; y < level1.layers[layerIdx].height; y++)
+        {
+            var idx = y * level1.layers[layerIdx].width + startX;
+            
+            for(var x = 0; x < level1.layers[layerIdx].width; x++)
+            {
+                if(level1.layers[layerIdx].data[idx] != 0)
+                {
+                    var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+                    var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * 
+                                (TILESET_TILE + TILESET_SPACING);
+                    var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * 
+                                (TILESET_TILE + TILESET_SPACING);
+                    context.drawImage(tileset,sx,sy,TILESET_TILE,TILESET_TILE,
+                                (x-startX)*TILE - offsetX, (y-1)*TILE,TILESET_TILE,TILESET_TILE);
+                }
+                idx++;
+            }
+        }
+    }
+}
+
+function run()
+{
+    context.fillStyle = "#ccc";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    
+    var deltaTime = getDeltaTime();
+    
+    player.update(deltaTime);
+    
+    drawMap();
+    player.draw();
+    
+    
+    
+    /*fpsTime += deltaTime;
+    fpsCount++;
+    if(fpsTime >= 1)
+    {
+        fpsTime -= 1;
+        fps = fpsCount;
+        fpsCount = 0;
+    }
+    
+    context.fillStyle = "#f00";
+    context.font = "14px Arial";
+    context.fillText("FPS: " + fps, 5, 20, 100);*/
+   
+    console.log(player.position);
+    console.log(player.velocity);
+    
+}
+
+
+
+function cellAtPixelCoord(layer, x,y)
+{
+    if(x < 0 || x > SCREEN_WIDTH || y < 0)
+        return 1;
+       
+    if(y > SCREEN_HEIGHT)
+        return 0;
+    return cellAtTileCoord(layer, p2t(x), p2t(y));
+};
+
+function cellAtTileCoord(layer, tx, ty)
+{
+	if(tx < 0 || tx >= Map.tw || ty < 0)
+		return 1;
+       
+	if(ty >= Map.th)
+		return 0;
+	return cells[layer][ty][tx];
+};
+
+function tileToPixel(tile)
+{
+    return tile * TILE;
+};
+
+function pixelToTile(pixel)
+{
+    return Math.floor(pixel/TILE);
+};
+
+function bound(value, min, max)
+{
+    if(value < min)
+        return min;
+    if(value > max)
+        return max;
+    return value;
+}
+
+var cells = [];
+function initialize() {
+    for(var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++) {
+        cells[layerIdx] = [];
+        var idx = 0;
+        for(var y = 0; y < level1.layers[layerIdx].height; y++) {
+            cells[layerIdx][y] = [];
+            for(var x = 0; x < level1.layers[layerIdx].width; x++) {
+                if(level1.layers[layerIdx].data[idx] != 0) {
+                    cells[layerIdx][y][x] = 1;
+                    cells[layerIdx][y-1][x] = 1;
+                    cells[layerIdx][y-1][x+1] = 1;
+                    cells[layerIdx][y][x+1] = 1;
+                }
+                else if(cells[layerIdx][y][x] != 1) {
+                    cells[layerIdx][y][x] = 0;
+                }
+                idx++;
+            }
+        }
+    }
+}
+
+initialize();
+
+
+(function() {
+    var onEachFrame;
+    if(window.requestAnimationFrame) {
+        onEachFrame = function(cb) {
+            var _cb = function() { cb(); window.requestAnimationFrame(_cb); }
+            _cb();
+        };
+    } else if (window.mozRequestAnimationFrame) {
+        onEachFrame = function(cb) {
+            var _cb = function() { cb();
+                window.mozRequestAnimationFrame(_cb); }
+                _cb();
+        };
+    } else {
+        onEachFrame = function(cb) {
+            setInterval(cb, 1000 / 60);
+        }
+    }
+    
+    window.onEachFrame = onEachFrame;
+})();
+
+window.onEachFrame(run);
